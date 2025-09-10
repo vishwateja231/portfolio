b951c10af5de5aeb1badc1412a2d3e9260b37f5c
vishwa teja <122215330+vishwateja231@users.noreply.github.com>
2025-09-11 01:36:27 +0530

    Update Frameworks.jsx

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -3,12 +3,10 @@ import { OrbitingCircles } from "./OrbitingCircles";
 export function Frameworks() {
   const skills = [
     "auth0",
-    "blazor",
-    "cplusplus",
-    "csharp",
+    "github",
+    "microsoftsqlserver",
+    "azure",
     "css3",
-    "dotnet",
-    "dotnetcore",
     "git",
     "html5",
     "javascript",
@@ -17,7 +15,6 @@ export function Frameworks() {
     "sqlite",
     "tailwindcss",
     "vitejs",
-    "wordpress",
   ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
